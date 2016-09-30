2e9d82fe3369bbeab7b4eddac0db75a8f27de512
mitch-rickman <mitchrickman@gmail.com>
2016-09-30 06:29:53 -0700

    Make file picker js module

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,3 @@
-let pickers = document.querySelectorAll('.host-dir-picker');
+let FilePicker = require('./modules/file-picker');
 
-for ( let i = 0, n = pickers.length; i < n; i++ ) {
-  pickers[i].addEventListener('change', function( e ) {
-    console.log( this.files[0].path );
-  })
-}
+let picker = new FilePicker();
---
+++ src/js/modules/file-picker.js
@@ -0,0 +1,14 @@
+class FilePicker {
+  constructor() {
+    console.log("new picker");
+    let pickers = document.querySelectorAll('.host-dir-picker');
+
+    for ( let i = 0, n = pickers.length; i < n; i++ ) {
+      pickers[i].addEventListener('change', function( e ) {
+        console.log( this.files[0].path );
+      });
+    }
+  }
+}
+
+module.exports = FilePicker;
