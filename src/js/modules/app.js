b01422bd2f8158bb9a60ccf525feaba292caf155
mitch-rickman <mitchrickman@gmail.com>
2016-10-04 22:14:35 -0700

    initialize host loop setup

--- src/js/modules/app.js
+++ src/js/modules/app.js
@@ -7,6 +7,7 @@ class App {
     console.log("app init");
     this.enableSettings();
     this.enableFilePickers();
+    this.listHosts();
   }
 
   /*
@@ -21,6 +22,39 @@ class App {
    */
   enableFilePickers( reset ) {
     this.pickerManger = new PickerManager('file-picker', 'selector', 'output');
+  }
+
+  /*
+   * get url base
+   * @return: base url string
+   */
+  getUrlBase() {
+    return "dev.local"
+  }
+
+  /*
+   * Get hosts from database
+   * @return: A list of saved hosts
+   */
+  getHosts() {
+    return [
+      {
+        hostName: "AwesomeSauce",
+        path: "/user/projects/awesome-sauce-incorporated/"
+      },
+      {
+        hostName: "AwesomeSauce",
+        path: "/user/projects/awesome-sauce-incorporated/"
+      }
+    ];
+  }
+
+  /*
+   * List hosts
+   */
+  listHosts() {
+    let baseurl = this.getUrlBase();
+    let hosts = this.getHosts();
   }
 }
 
